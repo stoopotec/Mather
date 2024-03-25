2053565a420c98b85d5436472e34d30484c40735
stoopotec <stoopotec@tuta.io>
2024-03-25 18:29:22 +0300

    initial

---
+++ src/build.sh
@@ -0,0 +1,1 @@
+g++ -Wall -g main.c -o server
---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+int main(int argc, char** argv) {
+
+    printf("server serve servant servirovka sera sreda\n");
+
+}
