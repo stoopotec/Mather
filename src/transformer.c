7cca20a7f645fae0acc16f839e895c43d8857a41
stoopotec <stoopotec@tuta.io>
2024-04-16 18:58:22 +0300

    symbols types and it interpritation

---
+++ src/transformer.c
@@ -0,0 +1,136 @@
+
+#include <stdio.h>
+#include "transformer.h"
+
+
+struct symbol_kv {
+    const char* key;
+    enum symbol_type value;
+};
+
+struct symbol_kv symbols_table[] = {
+
+    // { "NULL"               , NULL_SYMBOL }, 
+
+
+
+    { "⇒"                 , IMPLIES },
+    { "\\rightarrow"       , IMPLIES },
+    { "\\Rightarrow"       , IMPLIES },
+    { "\\imp"              , IMPLIES },
+
+    { "⇔"                 , EQUIVALENT },
+    { "\\eq"               , EQUIVALENT },
+
+    { "¬"                  , NOT },
+    { "\\not"              , NOT },
+
+    { "∧"                  , AND },
+    { "\\and"              , AND },
+    
+    { "∨"                  , OR },
+    { "\\or"               , OR },
+    
+    { "⊻"                  , XOR },
+    { "\\xor"              , XOR },
+
+
+
+    { "⊤"                  , TRUE },
+    { "\\top"              , TRUE },
+    { "\\true"             , TRUE },
+
+    { "⊥"                  , FALSE },
+    { "\\bot"              , FALSE },
+    { "\\false"            , FALSE },
+
+
+
+    { "∀"                  , FORALL },
+    { "\\forall"           , FORALL },
+
+    { "∃"                  , EXIST },
+    { "\\exist"            , EXIST },
+
+
+
+    { "("                  , BRACE_OPEN_ROUND },
+    { ")"                  , BRACE_CLOSE_ROUND },
+
+};
+#define LEN(x) (sizeof(x) / sizeof(*(x)))
+
+
+const char* get_string_from_symbol_type(enum symbol_type type) {
+    for (size_t i = 0; i < LEN(symbols_table); ++i) {
+        if (symbols_table[i].value == type) return symbols_table[i].key;
+    }
+    return "NULL";
+}
+
+
+
+struct symbol   get_next_symbol(const char* string, size_t* use_to) {
+    size_t start = 0, text_len = 0;
+
+    for (; string[start] != '\0'; ++start) if (string[start] != ' ') break;
+
+
+    for (size_t i = 0; i < LEN(symbols_table); ++i) {
+        int accept = 1;
+        text_len = 0;
+        for (size_t j = 0; symbols_table[i].key[j] != '\0'; ++j) {
+            if (symbols_table[i].key[j] != string[start + j]) {
+                accept = 0;
+                break;
+            }
+            if (symbols_table[i].key[j + 1] == '\0') text_len = j + 1;
+        }
+        if (accept) {
+            struct symbol symb = {
+                .type = symbols_table[i].value,
+                .text = string + start,
+                .text_len = text_len,
+            };
+            *use_to = start + text_len;
+            return symb;
+        };
+    }
+
+    struct symbol symb = {
+        .type = NULL_SYMBOL,
+        .text = NULL,
+        .text_len = 0,
+    };
+    *use_to = start;
+    return symb;
+}
+
+
+struct equation get_equation_from_string(const char* string) {
+
+    struct equation eq = {0};
+
+    size_t use_to = 0;
+
+    for (size_t i = 0; string[i] != '\0'; i += use_to) {
+        struct symbol next_symbol = get_next_symbol(string + i, &use_to);
+
+        printf("nextw symblol: %ld (%s), use_to: %ld, text_len: %ld\n", 
+            next_symbol.type, 
+            get_string_from_symbol_type(next_symbol.type), 
+            use_to, 
+            next_symbol.text_len
+        );
+
+        if (next_symbol.type) {
+            LIST_APPEND(next_symbol, eq.symbols, struct symbol);
+        }
+        else use_to = 1;
+    }
+
+
+    return eq;
+
+}
+
--- src/transformer.h
+++ src/transformer.h
@@ -1,24 +1,71 @@
 #pragma once
 
 #include <stdlib.h>
+#include "list.h"
 
 
 
+enum symbol_type {
+
+    NULL_SYMBOL                 = 0b000000000000000000000000000000000000000000000000,
 
 
+    IMPLIES                     = 0b100000000000000000000000000000000000101100000001,
+    EQUIVALENT                  = 0b100000000000000000000000000000000000100100000010,
+    NOT                         = 0b100000000000000000000000000000000000000100000011,
+    AND                         = 0b100000000000000000000000000000000000100000000100,
+    OR                          = 0b100000000000000000000000000000000000111000000101,
+    XOR                         = 0b100000000000000000000000000000000000011000000110,
+
+    TRUE                        = 0b010000000000000000000000000000000000000100000001,
+    FALSE                       = 0b010000000000000000000000000000000000000000000000,
+
+    FORALL                      = 0b110000000000000000000000000000000000000000000001,
+    EXIST                       = 0b110000000000000000000000000000000000000000000010,
+    // NOEXIST                     = 0b110000000000000000000000000000000000000000000011,
+
+    BRACE_OPEN_ROUND            = 0b001000000000000100000000000000000000000000000000,
+    BRACE_CLOSE_ROUND           = 0b001000000000000100000000000000000000000000000001,
+
+    // BRACE_OPEN_FIGURE           = 0b101000000000000000000000000000000000000000000000,
+    // BRACE_CLOSE_FIGURE          = 0b101000000000000000000000000000000000000000000001,
+
+
+
+    // VARIABLE                    = 0b000000000000000000000000000000000000000000000000,
+
+    // PLUS                        = 0b000000000000000000000000000000000000000000000000,
+    // MINUS                       = 0b000000000000000000000000000000000000000000000000,
+
+    // MULTIPLY                    = 0b000000000000000000000000000000000000000000000000,
+    // DIVIDE                      = 0b000000000000000000000000000000000000000000000000,
+
+};
+
+struct symbol {
+    enum symbol_type type;
+
+    const char* text;
+    size_t text_len;
+};
+typedef struct symbol symbol_t;
 
 
 struct equation {
-
+    LIST(symbol_t) symbols;
 };
 
 
 
-struct equation* get_equation_from_string(const char* string);
+const char* get_string_from_symbol_type(enum symbol_type type);
 
 
-struct equation* get_all_transformations(size_t* transformations, struct equation* equation);
-struct equation* get_all_transformations(size_t* transformations, const char* string);
+struct symbol   get_next_symbol(const char* string, size_t* use_to);
+struct equation get_equation_from_string(const char* string);
+
+
+struct equation get_all_transformations_s(size_t* transformations, const char* string);
+struct equation get_all_transformations(size_t* transformations, struct equation* equation);
 
 
 
