bea9d8d3c54a71877f3290b24d2a1872682c11cd
stoopotec <stoopotec@tuta.io>
2024-04-16 18:58:52 +0300

    new test

--- src/transformer_test.c
+++ src/transformer_test.c
@@ -9,11 +9,22 @@ int main() {
 
     while (1) {
 
+        printf("plasew entr a textuwu\n");
+
         fgets(buffer, sizeof(buffer), stdin);
 
-        size_t transformations;
+        // size_t transformations;
 
-        struct equation* equations = get_all_transformations(&transformations, buffer);
+        // struct equation* equations = get_all_transformations_s(&transformations, buffer);
+
+        struct equation eq = get_equation_from_string(buffer);
+        
+        printf("received: ");
+
+        for (size_t i = 0; i < eq.symbols.length; ++i) {
+            printf("%s ", get_string_from_symbol_type(eq.symbols.data[i].type));
+        }
+        printf("\n\n");
 
     }
 
