eaca9749f733803d95a30fed05cc53b66ebe9e6a
stoopotec <stoopotec@tuta.io>
2024-05-02 11:59:38 +0300

    перенёс код для toggle-switch в отдельные файлы

--- public/global.js
+++ public/global.js
@@ -2,24 +2,3 @@
 
 
 
-
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    const toggle_switches = document.querySelectorAll(".toggle-switch");
-
-    toggle_switches.forEach(item => {
-
-        item.addEventListener("click", (e) => {
-            
-            item.dispatchEvent(new CustomEvent("toggle", {
-                detail: {
-                    active: item.classList.toggle("active"),
-                },
-            }));
-            
-        });
-    });
-});
-
-
---
+++ public/toggle-switch.js
@@ -0,0 +1,20 @@
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const toggle_switches = document.querySelectorAll(".toggle-switch");
+
+    toggle_switches.forEach(item => {
+
+        item.addEventListener("click", (e) => {
+            
+            item.dispatchEvent(new CustomEvent("toggle", {
+                detail: {
+                    active: item.classList.toggle("active"),
+                },
+            }));
+            
+        });
+    });
+});
+
+
