91f94a849266b639d0bc6a11b69d82dfad6f46a8
stoopotec <stoopotec@tuta.io>
2024-05-02 11:56:09 +0300

    toggle-switch теперь крутой

--- public/global.js
+++ public/global.js
@@ -6,21 +6,20 @@
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    const toggle_switch = document.querySelectorAll('.toggle-switch');
+    const toggle_switches = document.querySelectorAll(".toggle-switch");
 
-    toggle_switch.forEach(item => {
-        item.addEventListener('click', (e) => {
+    toggle_switches.forEach(item => {
+
+        item.addEventListener("click", (e) => {
             
-            const circle = item.querySelector('.toggle-switch-inner');
-
-            circle.classList.toggle('active-inner');
-        
-            if (circle.classList.contains('active-inner')) {
-                item.style.background = '#3868a0';
-            } else {
-                item.style.background = '#ccc';
-            }
+            item.dispatchEvent(new CustomEvent("toggle", {
+                detail: {
+                    active: item.classList.toggle("active"),
+                },
+            }));
             
         });
-    }) 
-})
+    });
+});
+
+
